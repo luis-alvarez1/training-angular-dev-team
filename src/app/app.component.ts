d1303c7c5860bcddf0448320fd5d6f103863a7cd
Said Salcedo <said.salcedoc@unac.edu.co>
2021-10-21 17:32:08 -0500

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- texto = '';
+  listEstudiante: any[] = [
+    {nombre: 'Said Salcedo', estado: 'Promocionado'},
+    {nombre: 'Katherin Perez', estado: 'Regular'},
+    {nombre: 'Giancarlo Cabrera', estado: 'Regular'}
+  ]
 }
