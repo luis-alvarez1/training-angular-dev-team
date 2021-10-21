e1f4afab963470e351981131a944858a0363c1ad
Said Salcedo <said.salcedoc@unac.edu.co>
2021-10-21 17:01:26 -0500

    Two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- texto = 'Esto es un video sobre Event binding'
-
- cambiarTexto(): void{
-   this.texto = 'En el proximo vídeo vamos a ver Two way data-binding';
- }
+ texto = '';
 }
