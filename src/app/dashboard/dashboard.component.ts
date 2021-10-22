feb19b07fb726278f6f6019571e14b29e7b1d167
GiancarloCabrera <giancarlo.cabrera@unac.edu.co>
2021-10-21 19:54:30 -0500

    Creacion de componente de forma manual
    Creacion de archivos:
    .html
    .css
    .ts

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,21 @@
+//Tiene 3 partes
+
+//El importar el componente
+import { templateJitUrl } from "@angular/compiler";
+import { Component } from "@angular/core";
+
+//El componente
+@Component({
+    selector: 'app-dashboard',
+    templateUrl: "./dashboard.component.html",
+    styleUrls: ['./dashboard.component.css']
+})
+
+//La clase
+export class DashboardComponent {
+
+
+
+
+
+}
