bacc7d71c6d8dc74a446c79bc91c6e75d1b6ee5b
Said Salcedo <said.salcedoc@unac.edu.co>
2021-10-21 22:27:27 -0500

    Creaciones de componentes
    Practica de componentes: dashboard, footer, navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { TablaComponent } from './dashboard/tabla/tabla.component';
+import { ListaComponent } from './dashboard/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    DashboardComponent,
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    TablaComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
