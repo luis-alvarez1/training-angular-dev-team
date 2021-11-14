5cc656109bca8cb538516a42ee5d4367847eeef7
GiancarloCabrera <giancarlo.cabrera@unac.edu.co>
2021-11-14 18:27:00 -0500

    #9 Algoritmo 2
    Contruccion de una pantalla pequeña que reciba nombre, edad,dirección y peso. Habrá  un cuadro que aparezca cuando hayan mas de 1 paciente, se mostrará promedios de edad y peso, peso mayor y peso menor

--- src/app/pantalla/pantalla.component.ts
+++ src/app/pantalla/pantalla.component.ts
@@ -1,22 +1,90 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
+
+
+export class AppModule { }
 @Component({
   selector: 'app-pantalla',
   templateUrl: './pantalla.component.html',
   styleUrls: ['./pantalla.component.css']
 })
 export class PantallaComponent implements OnInit {
-  @Input() nombre: string = ''; 
-  @Input() edad: number = 0;
-  @Input() direccion: string = '';
-  @Input() peso: number = 0;
-
   
 
-  constructor() { }
+  public pacientes:  Array <any> = []
 
-  ngOnInit(): void {
-    var paciente = [this.nombre, this.edad, this.direccion, this.peso ];
+  public showInfo: Boolean = false;
+
+  formulario = new FormGroup({
+    nombre: new FormControl(''),
+    edad: new FormControl(undefined),
+    direccion: new FormControl(''),
+    peso: new FormControl(undefined)
+
+  })
+
+  promedioEdad(){
+    let sumatoria = 0;
+    this.pacientes.forEach(p => {
+      sumatoria += p.edad
+      
+    })
+
+
+    return sumatoria/this.pacientes.length;
   }
 
+  promedioPeso(){
+    let sumar = 0;
+    this.pacientes.forEach(s => {
+      sumar += s.peso
+      
+    });
+
+    return sumar/this.pacientes.length;
+  }
+
+  mayorPeso(){
+     let pesoM = 0;
+
+    this.pacientes.forEach(p => {
+      if(p.peso > pesoM){
+        pesoM = p.peso
+      }
+    });
+
+    return pesoM;
+  }
+
+  menorPeso(){
+  let pesoMenor = Number.MAX_SAFE_INTEGER;
+    this.pacientes.forEach(p=> {
+      if(p.peso< pesoMenor){
+        pesoMenor = p.peso;
+      }
+    });
+
+    return pesoMenor;
+  }
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+    
+  }
+  onSubmit() {
+    this.pacientes.push(this.formulario.value)
+    console.log(this.pacientes)
+
+    if(this.pacientes.length > 1){
+      this.showInfo = true;
+    }
+
+    
+  }
+  
 }
