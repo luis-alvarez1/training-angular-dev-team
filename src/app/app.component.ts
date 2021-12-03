9e1dab17683559487c03515c408edd234bda7dc8
Katherin Perez <katherinv.perezb@unac.edu.co>
2021-12-03 02:56:16 -0500

    Correcciones #2 componente numérico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ import Swal from 'sweetalert2';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  tituloDesdePadre = 'LISTADO CUMPLEAÑOS...';
+  colorDesdePadre = "red";
+  listadoPersonas: Personas[]; 
+  listadoDatos: string[] = ["Nombre", "Apellido", "Sexo", "Edad"];
 
   nuevoNumero: number = 0;
 
@@ -16,6 +20,11 @@ export class AppComponent {
   }
 
   constructor(){
+    this.listadoPersonas =  [
+      {nombre : "Katherin", apellido: "Perez", sexo : "Femenino", edad : 18},
+      {nombre : "Said", apellido: "Salcedo", sexo : "Masculino", edad : 19},
+      {nombre : "Juan Daniel", apellido: "Guerrero", sexo : "Masculino", edad : 17}
+    ];
   }
   mostrar(correo: string){
     Swal.fire(
